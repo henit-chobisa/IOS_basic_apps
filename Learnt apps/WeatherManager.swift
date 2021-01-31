de32cba8251e60566033acb494adf1faafc97e58
Henit Chobisa <henit.chobisa2020@vitstudet.ac.in>
2021-01-31 14:49:57 +0530

    updated

---
+++ Learnt apps/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Henit Work on 29/12/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Learnt apps/protocol/protocol/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  protocol
+//
+//  Created by Henit Work on 26/12/20.
+//
+class Bird {
+    
+    func fly() {
+        
+    }
+}
+
